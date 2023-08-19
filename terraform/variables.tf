dd177506f9d43dae03f3af1bb1928bde5bbccab9
neren <neren@sqli.com>
2023-08-19 12:09:24 +0200

    feat: tf - added state

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,19 @@ variable "linux_ami" {
   type        = string
   default     = "ami-030633f630317131c"
 }
+
+variable "region" {
+  description = "aws region"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "backend_bucket_name" {
+  type = string
+  default = "terraform-url-shortener-state"
+}
+
+variable "backend_locks" {
+  type = string
+  default = "url-shortener-locks"
+}
