eccbbdfe009033573ea99382d931099d855b92a9
neren <neren@sqli.com>
2023-08-24 17:38:06 +0200

    fix!: cors

--- router/router.go
+++ router/router.go
@@ -10,11 +10,12 @@ import (
 func Routes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(cors.Handler(cors.Options{
-		AllowOriginFunc:  AllowOriginFunc,
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "DELETE"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-PINGOTHER", "Referer", "Sec-Ch-Ua", "Sec-Ch-Ua-Mobile", "Sec-Ch-Ua-Platform", "User-Agent"},
-		ExposedHeaders:   []string{"*"},
-		AllowCredentials: true,
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
 		MaxAge:           300,
 	}))
 
@@ -27,8 +28,4 @@ func Routes() http.Handler {
 	r.Get("/health", appHandler.HealthCheck)
 
 	return r
-}
-
-func AllowOriginFunc(*http.Request, string) bool {
-	return true
 }
